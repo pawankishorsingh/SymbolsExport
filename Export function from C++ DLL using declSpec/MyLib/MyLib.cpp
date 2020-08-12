ceda837349744621b3d9dec02a02c0602ed5b6c6
Pawan Kishor Singh <pksingh@adobe.com>
2020-08-13 02:50:59 +0530

    Initial commit

---
+++ Export function from C++ DLL using declSpec/MyLib/MyLib.cpp
@@ -0,0 +1,6 @@
+#include "MyLib.h"
+#include <iostream>
+
+void dosomething() {
+	std::cout << "Executig the library method" << std::endl;
+}
---
+++ Export function from C++ DLL using declSpec/MyLib/MyLib.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//extern "C" will remove name mangling.
+extern "C" __declspec(dllexport) void dosomething();
